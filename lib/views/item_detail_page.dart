fbdf5616b5ba874d50970f388d837c1d81241ec0
Sungbin Yoon <sbyoon821@gmail.com>
2023-09-20 23:33:25 +0800

    Updated Item Detail Page

--- lib/views/item_detail_page.dart
+++ lib/views/item_detail_page.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
 import 'package:clovi_template/models/item_model.dart';
 import 'package:url_launcher/url_launcher_string.dart';
